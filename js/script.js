662f3b79a1030aa7213907255c1d194d39e4aa78
Bezobrazz <kostenko.uvd@gmail.com>
2022-07-07 20:30:22 +0300

    cards added

--- js/script.js
+++ js/script.js
@@ -103,6 +103,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
   function showModal() {
     modal.style.display = "block";
+    clearInterval(modalTimerId); //строка запрещает показывать модал по таймеру, если оно уже было вызвано
   }
   function hideModal() {
     modal.style.display = "none";
@@ -114,9 +115,7 @@ window.addEventListener("DOMContentLoaded", () => {
       document.body.style.overflow = "hidden"; //строчка отвечает за блокировку прокрутки страницы при открытом модале
     });
 
-    close.addEventListener("click", () => {
-      hideModal();
-    });
+    close.addEventListener("click", hideModal);
 
     modal.addEventListener("click", (e) => {
       if (e.target === modal) {
@@ -131,14 +130,86 @@ window.addEventListener("DOMContentLoaded", () => {
     });
   });
 
-  // btn.forEach((item) => {
-  //   item.addEventListener("click", () => {
-  //     modal.classList.add("show");
-  //     modal.classList.remove("hide");
-  //   });
-  // });
-  // close.addEventListener("click", () => {
-  //   modal.classList.add("hide");
-  //   modal.classList.remove("show");
-  // });
+  // const modalTimerId = setTimeout(showModal, 3000); //показывает модал через 3 сек
+
+  function showModalByScroll() {
+    if (
+      window.pageYOffset + document.documentElement.clientHeight >=
+      document.documentElement.scrollHeight - 1
+    ) {
+      showModal();
+      window.removeEventListener("scroll", showModalByScroll);
+    }
+  }
+
+  window.addEventListener("scroll", showModalByScroll); //функция показывает модал при скоролле до конца
+
+  // Используем классы для карточек
+
+  class MenuCard {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
+      this.src = src;
+      this.alt = alt;
+      this.title = title;
+      this.descr = descr;
+      this.price = price;
+      this.classes = classes;
+      this.parent = document.querySelector(parentSelector);
+      this.transfer = 27;
+      this.changeToUAH();
+    }
+
+    changeToUAH() {
+      this.price = this.price * this.transfer;
+    }
+
+    render() {
+      const element = document.createElement("div");
+      if (this.classes.length === 0) {
+        this.element = "menu__item";
+        element.classList.add(this.element);
+      } else {
+        this.classes.forEach((className) => element.classList.add(className));
+      }
+      element.innerHTML = `
+            <img src=${this.src} alt=${this.alt} />
+            <h3 class="menu__item-subtitle">${this.title}</h3>
+            <div class="menu__item-descr">
+              ${this.descr}
+            </div>
+            <div class="menu__item-divider"></div>
+            <div class="menu__item-price">
+              <div class="menu__item-cost">Цена:</div>
+              <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+            </div>  
+			`;
+      this.parent.append(element);
+    }
+  }
+  new MenuCard(
+    "img/tabs/vegy.jpg",
+    "vegy",
+    'Меню "Фитнес"',
+    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше',
+    9,
+    ".menu .container"
+  ).render();
+
+  new MenuCard(
+    "img/tabs/elite.jpg",
+    "elite",
+    "Меню “Премиум”",
+    "В меню “Премиум” мы используем не только красивый дизайн упаковки,",
+    12,
+    ".menu .container"
+  ).render();
+
+  new MenuCard(
+    "img/tabs/post.jpg",
+    "post",
+    'Меню "Постное"',
+    "Меню “Постное” - это тщательный подбор ингредиентов: полное",
+    17,
+    ".menu .container"
+  ).render();
 });
