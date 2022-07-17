de57ee3781b159c04522a189563aa4456d519a44
Bezobrazz <kostenko.uvd@gmail.com>
2022-07-17 08:37:05 +0300

    variant of courusel added

--- js/script.js
+++ js/script.js
@@ -196,8 +196,21 @@ window.addEventListener("DOMContentLoaded", () => {
     return await res.json();
   };
 
-  getResource("http://localhost:3000/menu").then((data) => {
-    data.forEach(({ img, altimg, title, descr, price }) => {
+  // getResource("http://localhost:3000/menu").then((data) => {
+  //   data.forEach(({ img, altimg, title, descr, price }) => {
+  //     new MenuCard(
+  //       img,
+  //       altimg,
+  //       title,
+  //       descr,
+  //       price,
+  //       ".menu .container"
+  //     ).render();
+  //   });
+  // });
+
+  axios.get("http://localhost:3000/menu").then((data) => {
+    data.data.forEach(({ img, altimg, title, descr, price }) => {
       new MenuCard(
         img,
         altimg,
@@ -208,33 +221,6 @@ window.addEventListener("DOMContentLoaded", () => {
       ).render();
     });
   });
-
-  // new MenuCard(
-  //   "img/tabs/vegy.jpg",
-  //   "vegy",
-  //   'Меню "Фитнес"',
-  //   'Меню "Фитнес" - это новый подход к приготовлению блюд: больше',
-  //   9,
-  //   ".menu .container"
-  // ).render();
-
-  // new MenuCard(
-  //   "img/tabs/elite.jpg",
-  //   "elite",
-  //   "Меню “Премиум”",
-  //   "В меню “Премиум” мы используем не только красивый дизайн упаковки,",
-  //   12,
-  //   ".menu .container"
-  // ).render();
-
-  // new MenuCard(
-  //   "img/tabs/post.jpg",
-  //   "post",
-  //   'Меню "Постное"',
-  //   "Меню “Постное” - это тщательный подбор ингредиентов: полное",
-  //   17,
-  //   ".menu .container"
-  // ).render();
 
   //Forms
   const forms = document.querySelectorAll("form");
@@ -318,4 +304,115 @@ window.addEventListener("DOMContentLoaded", () => {
   fetch("http://localhost:3000/menu")
     .then((data) => data.json())
     .then((res) => console.log(res));
+
+  //Slider
+  const slides = document.querySelectorAll(".offer__slide"),
+    prev = document.querySelector(".offer__slider-prev"),
+    next = document.querySelector(".offer__slider-next"),
+    total = document.querySelector("#total"),
+    current = document.querySelector("#current"),
+    slidesWrapper = document.querySelector(".offer__slider-wrapper"),
+    slidesField = document.querySelector(".offer__slider-inner"),
+    width = window.getComputedStyle(slidesWrapper).width;
+  let slideIndex = 1;
+  let offset = 0;
+
+  if (slides.length < 10) {
+    total.textContent = `0${slides.length}`;
+    current.textContent = `0${slideIndex}`;
+  } else {
+    total.textContent = slides.length;
+    current.textContent = slideIndex;
+  }
+
+  slidesField.style.width = 100 * slides.length + "%";
+  slidesField.style.display = "flex";
+  slidesField.style.transition = "0.5s all";
+
+  slidesWrapper.style.overflow = "hidden";
+
+  slides.forEach((slide) => {
+    slide.style.width = width;
+  });
+
+  next.addEventListener("click", function () {
+    if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+      offset = 0;
+    } else {
+      offset += +width.slice(0, width.length - 2);
+    }
+    slidesField.style.transform = `translateX(-${offset}px)`;
+
+    if (slideIndex == slides.length) {
+      slideIndex = 1;
+    } else {
+      slideIndex++;
+    }
+
+    if (slides.length < 10) {
+      current.textContent = `0${slideIndex}`;
+    } else {
+      current.textContent = slideIndex;
+    }
+  });
+
+  prev.addEventListener("click", function () {
+    if (offset == 0) {
+      offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+    } else {
+      offset -= +width.slice(0, width.length - 2);
+    }
+    slidesField.style.transform = `translateX(-${offset}px)`;
+
+    if (slideIndex == 1) {
+      slideIndex = slides.length;
+    } else {
+      slideIndex--;
+    }
+
+    if (slides.length < 10) {
+      current.textContent = `0${slideIndex}`;
+    } else {
+      current.textContent = slideIndex;
+    }
+  });
+
+  // showSlides(slideIndex); // инициализируем слайдер
+
+  // if (slides.length < 10) {
+  //   total.textContent = `0${slides.length}`;
+  // } else {
+  //   total.textContent = slides.length;
+  // }
+
+  // function showSlides(n) {
+  //   //n - это начальный индекс т.е. 1
+  //   if (n > slides.length) {
+  //     slideIndex = 1; // когда доходим до конца слайдов перемещаемся на 1
+  //   }
+
+  //   if (n < 1) {
+  //     slideIndex = slides.length;
+  //   }
+
+  //   slides.forEach((item) => (item.style.display = "none")); // скрываем все слайды
+  //   slides[slideIndex - 1].style.display = "block"; // - 1 чтобы показывать с 0 слайда
+
+  //   if (slides.length < 10) {
+  //     current.textContent = `0${slideIndex}`;
+  //   } else {
+  //     current.textContent = slideIndex;
+  //   }
+  // }
+
+  // function plusSlides(n) {
+  //   showSlides((slideIndex += n)); //функция позволяющая показывать индекс(нумерацию) отображаемого слайда
+  // }
+
+  // prev.addEventListener("click", function () {
+  //   plusSlides(-1);
+  // });
+  // next.addEventListener("click", function () {
+  //   plusSlides(1);
+  // });
 });
